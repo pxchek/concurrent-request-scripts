989dbab028222eb0bafbc99a5124e4f5b2a6e268
Panindra Chekoori <70819131+pxchek@users.noreply.github.com>
2023-01-07 14:10:50 -0600

    Reduce lock contention comments

--- curlHttpRequests.sh
+++ curlHttpRequests.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Lock contention is influenced by two factors,
+# 1. How often is the lock requested by threads running on-CPU ? 
+# 2. How long are the locks held once required ?
+# ***Locks are mostly uncontented when the product of the above 2 questions is sufficiently small.***
+
+# Reduce duration of locks held, frequency of lock requests and replace exclusive locks in favor of concurrency friendly collections, read-write locks, 
+# immutable objects and atomic variables. 
+
 # curlHttpRequests.sh 5 732C35D59030443DAEEE5996D1CA3476
 
 # Simulates multiple calls to recreate thread contention issues on the back-end such as java.lang.Thread.State: BLOCKED (on object monitor)
